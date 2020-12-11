b1868d9f74c9f8788956f2916318271c876a0ef1
Heltzel <heltzel.service@hotmail.com>
2020-12-11 15:37:51 +0100

    Banner Component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Banner from '../components/Banner'
 import Hero from '../components/Hero'
 
 export default function Home() {
-  return <Hero hero="defaultHero" />
+  return (
+    <Hero hero="defaultHero">
+      <Banner
+        title="luxurious rooms"
+        subtitle="deluxe rooms starting at &euro;299,-"
+      >
+        <Link to="/rooms" className="btn-primary">
+          our rooms
+        </Link>
+      </Banner>
+    </Hero>
+  )
 }
--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -1,6 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import Hero from '../components/Hero'
+import Banner from '../components/Banner'
 
 export default function Rooms() {
-  return <Hero hero="roomsHero" />
+  return (
+    <Hero hero="roomsHero">
+      <Banner title="our rooms" subtitle="">
+        <Link to="/" className="btn-primary">
+          back to homepage
+        </Link>
+      </Banner>
+    </Hero>
+  )
 }
