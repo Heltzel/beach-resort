65cbe7dce459979d1ba9a5feb2a4073701603f6a
Heltzel <heltzel.service@hotmail.com>
2020-12-11 13:58:35 +0100

    Navbar Component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Error from './pages/Error'
 import Home from './pages/Home'
 import Rooms from './pages/Rooms'
 import SingleRoom from './pages/SingleRoom'
+import Navbar from './Navbar'
 
 import { Route, Switch } from 'react-router-dom'
 
 function App() {
   return (
     <>
+      <Navbar />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/rooms/" component={Rooms} />
