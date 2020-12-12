3b12d217d615ada9494e05a222941f0d7fd3c2c9
Heltzel <heltzel.service@hotmail.com>
2020-12-12 18:04:54 +0100

    Render single room complete

--- README.md
+++ README.md
@@ -1,16 +1,32 @@
-#  React Beach Resort
-## Features:
-<ul>
-<li> React Router Dom</li>
-<li> React Icons</li>
-</ul>
+# React Beach Resort
+<div>
+    <h2> Objective:</h2>
+    Display hotel rooms of a beach resort with filter functionality.
+</div>
+<hr />
+<div>
+  <h2> Features:</h2>
+  <ul>
+    <li> React Router Dom</li>
+    <li> React Icons</li>
+    <li> React context API</li>
+    <li> Styled Components</li>
+  </ul>
+</div>
+<hr />
+<div>
+  <h2>Docs:</h2>
 
-***
-
-## Docs:
-https://reactrouter.com/web/guides/quick-start <br />
-https://react-icons.github.io/react-icons/ <br />
-https://reactjs.org/docs/context.html<br />
-https://styled-components.com/ <br />
-
-***
+  | subject               | Link                                           | 
+  | -------------         |:-------------                                  | 
+  |  React Router Dom     | https://reactrouter.com/web/guides/quick-start |
+  |  React Icons          | https://react-icons.github.io/react-icons/     | 
+  |  React context API    | https://reactjs.org/docs/context.html          | 
+  |  Styled Components    | https://styled-components.com/                 | 
+</div>
+<hr />
+<style
+  type="text/css">
+h1 {color: #426ef0;}
+div{padding: 1rem 0;}
+</style>
--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -45,14 +45,48 @@ export default class SingleRoom extends Component {
       pets,
       images,
     } = room
+    const [mainImg, ...defaultImg] = images
     return (
-      <StyledHero img={images[0] || this.state.defaultBcg}>
-        <Banner title={`${name} room`}>
-          <Link to="/rooms" className="btn-primary">
-            back to rooms
-          </Link>
-        </Banner>
-      </StyledHero>
+      <>
+        <StyledHero img={mainImg || this.state.defaultBcg}>
+          <Banner title={`${name} room`}>
+            <Link to="/rooms" className="btn-primary">
+              back to rooms
+            </Link>
+          </Banner>
+        </StyledHero>
+        <section className="single-room">
+          <div className="single-room-images">
+            {defaultImg.map((image, index) => {
+              return <img src={image} key={index} alt={name} />
+            })}
+          </div>
+          <div className="single-room-info">
+            <article className="description">
+              <h3>Details</h3>
+              <p>{description}</p>
+            </article>
+            <article className="info">
+              <h3>info</h3>
+              <h6>price: &euro;{price},-</h6>
+              <h6>size: {size} SQFT</h6>
+              <h6>
+                max capacity: {capacity > 1 ? `${capacity} people` : '1 person'}
+              </h6>
+              <h6>{pets ? 'pets allowed' : 'no pets allowed'}</h6>
+              <h6>{breakfast && 'free breakfast included'}</h6>
+            </article>
+          </div>
+        </section>
+        <section className="room-extras">
+          <h6>extras</h6>
+          <ul className="extras">
+            {extras.map((item, index) => {
+              return <li key={index}> -{item}</li>
+            })}
+          </ul>
+        </section>
+      </>
     )
   }
 }
