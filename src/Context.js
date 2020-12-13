ff7817ed6f5600e670ac1855962fafb85b85196a
Heltzel <heltzel.service@hotmail.com>
2020-12-13 14:10:28 +0100

    Filter room type fuctionality

--- src/Context.js
+++ src/Context.js
@@ -10,6 +10,15 @@ class RoomProvider extends Component {
     sortedRooms: [],
     featuredRooms: [],
     loading: true,
+    type: 'all',
+    capacity: 1,
+    price: 0,
+    minprice: 0,
+    maxPrice: 0,
+    minSize: 0,
+    maxSize: 0,
+    breakfast: false,
+    pets: false,
   }
   // getData()
 
@@ -17,11 +26,17 @@ class RoomProvider extends Component {
     // this.getData
     let rooms = this.formatData(items)
     let featuredRooms = rooms.filter((room) => room.featured)
+    let maxPrice = Math.max(...rooms.map((room) => room.price))
+    let maxSize = Math.max(...rooms.map((room) => room.size))
+
     this.setState({
       rooms,
       featuredRooms,
       sortedRooms: rooms,
       loading: false,
+      price: maxPrice,
+      maxPrice,
+      maxSize: maxSize,
     })
   }
   formatData(items) {
@@ -39,9 +54,46 @@ class RoomProvider extends Component {
     return tempRooms.find((room) => room.slug === slug)
   }
 
+  handleChange = (e) => {
+    const target = e.target
+    const value = e.type === 'checkbox' ? target.checked : target.value
+    const name = e.target.name
+    this.setState(
+      {
+        [name]: value,
+      },
+      this.filterrooms,
+    )
+  }
+
+  filterrooms = () => {
+    let {
+      rooms,
+      type,
+      capacity,
+      price,
+      minSize,
+      maxSize,
+      breakfast,
+      pets,
+    } = this.state
+    let tempRooms = [...rooms]
+    if (type !== 'all') {
+      tempRooms = tempRooms.filter((room) => room.type === type)
+    }
+    this.setState({
+      sortedRooms: tempRooms,
+    })
+  }
   render() {
     return (
-      <RoomContext.Provider value={{ ...this.state, getRoom: this.getRoom }}>
+      <RoomContext.Provider
+        value={{
+          ...this.state,
+          getRoom: this.getRoom,
+          handleChange: this.handleChange,
+        }}
+      >
         {this.props.children}
       </RoomContext.Provider>
     )
--- src/components/RoomsFilter.js
+++ src/components/RoomsFilter.js
@@ -1,5 +1,54 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { RoomContext } from '../Context'
+import Title from '../components/Title'
 
-export default function RoomsFilter() {
-  return <div> hello from rooms Filter</div>
+// get all unique values
+const getUniquevalues = (items, value) => {
+  return [...new Set(items.map((item) => item[value]))]
+}
+export default function RoomsFilter({ rooms }) {
+  const context = useContext(RoomContext)
+  const {
+    handleChange,
+    type,
+    capacity,
+    price,
+    minprice,
+    maxPrice,
+    minSize,
+    maxSize,
+    breakfast,
+    pets,
+  } = context
+
+  let types = getUniquevalues(rooms, 'type')
+  types = ['all', ...types]
+  types = types.map((type, index) => {
+    return (
+      <option value={type} key={index}>
+        {type}
+      </option>
+    )
+  })
+  let capacities = getUniquevalues(rooms, 'capacity')
+
+  return (
+    <section className="filter-container">
+      <Title title="search rooms" />
+      <form className="filter-form">
+        <div className="form-group">
+          <label htmlFor="type">room type</label>
+          <select
+            name="type"
+            id="type"
+            value={type}
+            className="form-control"
+            onChange={handleChange}
+          >
+            {types}
+          </select>
+        </div>
+      </form>
+    </section>
+  )
 }
